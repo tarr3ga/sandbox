f7d027a3d5393c4a91b4319481f72ddc122089d3
Andy Myers <mail@jamyers.net>
2019-02-08 11:09:38 -0500

    Added GoogleChromeTabs project

---
+++ GetChromeTabs/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Automation;
+
+
+namespace GetChromeTabs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] procsChrome = Process.GetProcessesByName("chrome");
+
+            if(procsChrome.Length <= 0)
+            {
+                Console.WriteLine("Chrome is not running");
+            }
+            else
+            {
+                foreach(Process proc in procsChrome)
+                {
+                    if(proc.MainWindowHandle == IntPtr.Zero)
+                    {
+                        continue;
+                    }
+
+                    AutomationElement root = AutomationElement.FromHandle(proc.MainWindowHandle);
+
+                    Condition condNewTab = new PropertyCondition(AutomationElement.NameProperty, "New Tab");
+
+                    AutomationElement elmNewTab = root.FindFirst(TreeScope.Descendants, condNewTab);
+
+                    TreeWalker treeWalker = TreeWalker.ControlViewWalker;
+
+                    AutomationElement elmTabStrip = treeWalker.GetParent(elmNewTab);
+
+                    Condition condTabItem = new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.TabItem);
+
+                    foreach (AutomationElement tabitem in elmTabStrip.FindAll(TreeScope.Children, condTabItem))
+                    {
+                        Console.WriteLine(tabitem.Current.Name);
+                    }
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ GoogleAPI/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Google.Apis.Discovery.v1;
+using Google.Apis.Discovery.v1.Data;
+using Google.Apis.Services;
+
+namespace GoogleAPI
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Discovery API Sample");
+            Console.WriteLine("====================");
+            try
+            {
+                new Program().Run().Wait();
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Console.WriteLine("ERROR: " + e.Message);
+                }
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private async Task Run()
+        {
+            // Create the service.
+            var service = new DiscoveryService(new BaseClientService.Initializer
+            {
+                ApplicationName = "Discovery Sample",
+                ApiKey = "AIzaSyBZ1L7rEBFokfhcsSQC6E23qQObWdZEdkI",
+            });
+
+            // Run the request.
+            Console.WriteLine("Executing a list request...");
+            var result = await service.Apis.List().ExecuteAsync();
+
+            // Display the results.
+            if (result.Items != null)
+            {
+                foreach (DirectoryList.ItemsData api in result.Items)
+                {
+                    Console.WriteLine(api.Id + " - " + api.Title);
+                }
+            }
+        }
+    }
+}
